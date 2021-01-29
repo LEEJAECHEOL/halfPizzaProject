64cfb31a67a3c268a71a5b14ca86c526d2ab635e
DongTaeWan <bdj42@naver.com>
2021-01-29 14:28:46 +0900

    이미지업로드 수정본 아이디 비밀번호 찾기

--- src/com/cos/halfPizza/filter/ImageUploadFilter.java
+++ src/com/cos/halfPizza/filter/ImageUploadFilter.java
@@ -3,8 +3,9 @@ package com.cos.halfPizza.filter;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.Enumeration;
+import java.util.Random;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -19,6 +20,17 @@ import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 public class ImageUploadFilter implements Filter {
+	
+	public static String getrndnum(int loopcount){
+		String str = "";
+		int d = 0;
+		for (int i = 1; i <= loopcount; i++){
+			Random r = new Random();
+			d = r.nextInt(9);
+			str = str + Integer.toString(d);
+		}
+		return str;
+	}
 
    @Override
    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -27,7 +39,6 @@ public class ImageUploadFilter implements Filter {
 
 		HttpServletRequest req = (HttpServletRequest)request;
 		HttpServletResponse resp = (HttpServletResponse)response;
-		
 		String contentType = null;
 		if(req.getContentType() != null) {
 			contentType = (req.getContentType()).split(";")[0];
@@ -37,45 +48,43 @@ public class ImageUploadFilter implements Filter {
 			String endPoint = req.getRequestURI().replaceAll(req.getContextPath(), "");
 			int maxSize = 1024 * 1024 * 10;	//10M
 			String path = "";
-			if(endPoint.equals("/admin/menu/registProc")) {
+			if(endPoint.equals("/admin/menu/registProc") || endPoint.equals("/admin/menu/updateProc")) {
 				path = "/images/menu/";
 			}
-			else if(endPoint.equals("/admin/menu/updateProc")) {
-				path = "/images/menu/";
-			}
-			else if(endPoint.equals("/admin/event/registProc")) {
-				path = "/images/event/";
-			}
-			else if(endPoint.equals("/admin/event/updateProc")) {
+			else if(endPoint.equals("/admin/event/registProc") || endPoint.equals("/admin/event/updateProc")) {
 				path = "/images/event/";
 			}
 			String uploadPath = req.getSession().getServletContext().getRealPath(path);
-			System.out.println("업로드 패스: "+uploadPath);
-			// �씠誘몄� �뾽濡쒕뱶
+			
 			try {
 				MultipartRequest multi = new MultipartRequest(req, uploadPath, maxSize, "utf-8", new DefaultFileRenamePolicy());
-				String gubun = multi.getParameter("gubun");
+				String[] gubun = multi.getParameterValues("gubun");
 				
 				Enumeration<?> params = multi.getParameterNames();
+				
 				while(params.hasMoreElements()) {
 					String param = (String)params.nextElement();
 					req.setAttribute(param, multi.getParameter(param));
-					System.out.println(multi.getParameter(param));
 				}
+				req.setAttribute("path", path);
 				
 				Enumeration<?> files = multi.getFileNames();
-		        String file = (String)files.nextElement();
-		        String fileName = multi.getFilesystemName(file);
-		        SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
-		        Calendar time = Calendar.getInstance();   
-		        String type = (multi.getContentType(file)).split("/")[1]; 
-		        String newFileName = gubun + "_" + format.format(time.getTime()) + '.' + type;
-		        File oldFile = new File(uploadPath + fileName);
-		        File newFile = new File(uploadPath + newFileName);
-		        oldFile.renameTo(newFile); 
-		        req.setAttribute("originFileName", fileName);
-		        req.setAttribute("changeFileName", newFileName);
-		        req.setAttribute("path", path);
+				
+				int i = 1;
+				while(files.hasMoreElements()) {
+					String file = (String)files.nextElement();
+					String fileName = multi.getFilesystemName(file);
+					SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss_SS");
+					String type = (multi.getContentType(file)).split("/")[1];
+					String newFileName = gubun[i - 1] + "_" + format.format(new Date())+ '_' + getrndnum(10) + '.' + type;
+					File oldFile = new File(uploadPath + fileName);
+			        File newFile = new File(uploadPath + newFileName);
+			        oldFile.renameTo(newFile);
+					req.setAttribute("originFileName" + i, fileName);
+			        req.setAttribute("changeFileName" + i, newFileName);
+			        req.setAttribute("path", path);
+					i++;
+				}
 		        
 			} catch (Exception e) {
 				Script.back(resp, "파일의 크기가 너무 큽니다.");
